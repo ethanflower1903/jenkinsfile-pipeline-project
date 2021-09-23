594b008cbe821f4832dc9884a8b2a65df26f8389
Ethanflower1903 <flowerethan@aol.com>
2021-09-23 11:24:57 -0700

    updated jenkinsfile and added pipeline.py

--- pipline.py
+++ pipline.py
@@ -1,10 +1,12 @@
 pipeline {
     agent any
     stages {
-        stage('build') {
+        stage('run') {
             steps {
                 echo 'Clarusway_Way to Reinvent Yourself'
-                sh 'echo Integrating Jenkins Pipeline with GitHub Webhook using Jenkinsfile'
+                sh 'python --version'
+                sh 'python pipeline.py'
             }
         }
     }
+}
